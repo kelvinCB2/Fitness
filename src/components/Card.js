ab0fa005dfe8a1d315ef439d6885d0c68c155888
Kelvin Calcaño <kelvin2original@hotmail.com>
2020-09-13 18:24:36 -0400

    Video 11, Curso de ReactJS, Las Props. Pasarle parametros a los componentes, a los css, al html, a todo. Haciendo dinamico nuestros componentes y cargando imagenes desde el internet.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import ExcerciseImage from '../images/exercise.png'
+import CirclesImage from '../images/circles.png'
 import './styles/Card.css'
 
 
 class Card extends React.Component {
 
     render() {
+        const {title,description,image,leftColor,rightColor} = this.props
         return (
-            <div className="card mx-auto Fitness-Card"> 
+            <div className="card mx-auto Fitness-Card"
+                style={{
+                    backgroundImage: `url(${CirclesImage}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+                }}
+            >
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={ExcerciseImage} className="float-right"></img>
+                            <img src={image} className="float-right"></img>
                         </div>
                         <div className="col-6 Fitness-Card-Info">
-                            <h1>Technique Guides</h1>
-                            <p>Learn amazing street workout and calisthenics</p>
+                            <h1>{title}</h1>
+                            <p>{description}</p>
                         </div>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -15,4 +15,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 const container = document.getElementById('root')
 
 // ReactDOM.render(Que Renderear, Donde Renderear)
-ReactDOM.render(<Card/>, container)
+ReactDOM.render(<Card
+  title="This is my title"
+  description="Learn amazing street workout and calisthenics"
+  image="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+  leftColor="#A74CF2"
+  rightColor="#617BFB"
+/>, container)
