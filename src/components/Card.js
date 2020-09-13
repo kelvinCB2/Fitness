d8ab2edf7909c0724b861ed856b52f32f3183ca9
Kelvin Calcaño <kelvin2original@hotmail.com>
2020-09-13 17:44:08 -0400

    Video 10, Curso de ReactJS, Agregando css a componentes. Ponerle css a nuestro proyecto, instalar o importar bootstrap, usar bootstrap

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import ExcerciseImage from '../images/exercise.png'
+import './styles/Card.css'
+
 
 class Card extends React.Component {
 
     render() {
         return (
-            <div>
-                <div>
-                    <img src={ExcerciseImage}></img>
-                </div>
-                <div>
-                    <h1>Technique Guides</h1>
-                    <p>Learn amazing street workout and calisthenics</p>
+            <div className="card mx-auto Fitness-Card"> 
+                <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                            <img src={ExcerciseImage} className="float-right"></img>
+                        </div>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>Technique Guides</h1>
+                            <p>Learn amazing street workout and calisthenics</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
