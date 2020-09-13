8b0c1099ff1821492f0e8ff8ae38d7bb54e993a2
Kelvin Calcaño <kelvin2original@hotmail.com>
2020-09-13 16:10:09 -0400

    Video 06, Curso de ReactJS, JSX.   Creando objetos, funciones, img, h1, entendiendo JSX, Contenedores, etc

--- src/index.js
+++ src/index.js
@@ -10,9 +10,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = <h1>Hello React</h1>
+
+const name = 'Kelvin'
+
+const user = {
+
+  firstName: "Kelvin",
+  lastName: "Calcano",
+  avatar: "http://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png"
+
+}
+
+function getName(user) {
+  return `${user.firstName} ${user.lastName}`
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello {getName(user)}</h1>
+  } else {
+    return <h1>Hello Stranger</h1>
+  }
+}
+
+const element = (
+  <div>
+    <h1>{getGreeting(user)}</h1>
+    <img src={user.avatar}></img>
+  </div>
+)
 const container = document.getElementById('root')
 
 // ReactDOM.render(Que Renderear, Donde Renderear)
 
-ReactDOM.render(element,container)
+ReactDOM.render(element, container)
