60ac7c70b1c673704bb25d7be7b0cf13c43a7885
Kelvin Calcaño <kelvin2original@hotmail.com>
2020-09-19 06:41:46 -0400

    Video12, Curso de ReactJS, El State.  Creandole un estado inicial al component, es decir un state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import ExcerciseImage from '../images/exercise.png'
+//import ExcerciseImage from '../images/exercise.png'
 import CirclesImage from '../images/circles.png'
 import './styles/Card.css'
 
 
 class Card extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            image: 'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/bulbasaur.png?alt=media&token=567caf19-af47-414e-a9d4-3854ab24c7dc'
+        }
+    }
 
+    //se va a ejecutar despues de que un componente se monte
+    componentDidMount(){
+        setTimeout(() =>{
+            this.setState({
+                image: ''
+            })
+        }, 5000)
+    }
     render() {
         const {title,description,image,leftColor,rightColor} = this.props
         return (
@@ -17,7 +31,7 @@ class Card extends React.Component {
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={image} className="float-right"></img>
+                            <img src={this.state.image} alt="trucho" className="float-right"></img>
                         </div>
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
