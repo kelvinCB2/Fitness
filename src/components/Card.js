409c5590bce1a254f74275ee6c3a4a510df92cda
kelvinCB <kelvinr02@hotmail.com>
2020-09-24 17:20:58 -0400

    Creacion y consumo de api local, card components diversos

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,23 +5,9 @@ import './styles/Card.css'
 
 
 class Card extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            image: 'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/bulbasaur.png?alt=media&token=567caf19-af47-414e-a9d4-3854ab24c7dc'
-        }
-    }
-
-    //se va a ejecutar despues de que un componente se monte
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                //En el video 13 elimine la imagen del state y aprendi mucha teoria.   
-            })
-        }, 5000)
-    }
+   
     render() {
-        const { title, description, leftColor, rightColor } = this.props
+        const { title, description, image, leftColor, rightColor } = this.props
         return (
             <div className="card mx-auto Fitness-Card"
                 style={{
@@ -31,7 +17,7 @@ class Card extends React.Component {
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={this.props.image} alt="trucho" className="float-right"></img>
+                            <img src={image} alt="trucho" className="float-right"></img>
                         </div>
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
